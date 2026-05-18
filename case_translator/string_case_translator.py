9f5d8d5708e1990dec03db03f4b4a3ca7acb7ec5
unknown <budagyanvahe@gmail.com>
2026-05-18 21:42:23 +0300

    Upgrade docstring in function translate_string_case() by adding Usage Example, Features and Unique Selling Proposition (USP).

--- case_translator/string_case_translator.py
+++ case_translator/string_case_translator.py
@@ -11,7 +11,28 @@ def translate_string_case(*,
     """
     This function gets variable name, its current case and wanted case (the case you need) and **returns name of variable in wanted case**.
     Supported registries are: **camel**, **kebab**, **pascal**, **screaming**, **snake**. 
-    If function gets arguments that are not in supported cases, it **returns None**.
+    If function gets arguments that are not in supported cases, it **returns None**. 
+
+    Usage Example
+    -------------
+
+    print(translate_string_case(variable_name="SCREAMING_TO_PASCAL", current_case="screaming", wanted_case="pascal"))
+    >>> ScreamingToPascal
+    
+    print(translate_string_case(variable_name="camelToKebab", current_case="is_is_obviously_camel_dont_you_see", wanted_case="just_read_the_last_word"))
+    >>> None
+
+    Features
+    --------
+
+    Library string_case_translator uses the WET pattern, so a **single point of failure is impossible**. 
+    
+    This function uses built-in C-methods in python, so **it works faster** than regular expressions. 
+
+    Unique Selling Proposition (USP)
+    ----------------------------
+
+    This function and library propose **high speed, easy usage and localized potential bugs**. 
     """
     
     try:
